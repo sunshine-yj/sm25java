9dcc8ba24f6504a1e3a9ec0326f7ec813ef37aa0
Kimyoujin <rladbwls3212003@naver.com>
2025-07-02 14:24:36 +0900

    0702 - (Control2.java 수정)

--- day01/src/control/Control2.java
+++ day01/src/control/Control2.java
@@ -6,11 +6,13 @@ public class Control2 {
         // for 문을 이용하여 구하시오
 
         int sum = 0;
-        double avg = 0;
+        double avg = 0.0;
+        int count = 0;
         for (int i = 1; i <= 10; i++) {
             sum += i;
+            count++;
         }
-        avg = sum / 10.0;
+        avg = sum / (double)count;
         System.out.printf("합계 : %d, 평균 : %.1f", sum, avg);
     }
 }
