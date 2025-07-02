999b23165871792ee066fce89d4ce6eeea5fad06
Kimyoujin <rladbwls3212003@naver.com>
2025-07-02 14:30:46 +0900

    0702 - (Control2.java 수정)

--- day01/src/control/Control2.java
+++ day01/src/control/Control2.java
@@ -4,15 +4,14 @@ public class Control2 {
     public static void main(String[] args) {
         // 1~10까지의 합과 평균을
         // for 문을 이용하여 구하시오
-
         int sum = 0;
-        double avg = 0.0;
         int count = 0;
-        for (int i = 1; i <= 10; i++) {
-            sum += i;
-            count++;
+        double avg = 0.0;
+
+        for (count = 1; count <= 10; count++) {
+            sum += count;
         }
-        avg = sum / (double)count;
-        System.out.printf("합계 : %d, 평균 : %.1f", sum, avg);
+        avg = sum / Double.valueOf(count);
+        System.out.printf("합계 : %d, 평균 : %5.2f", sum, avg);
     }
 }
