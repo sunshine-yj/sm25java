b52a8bec5b9888519917d1cf9d4c9e6bc030c812
Kimyoujin <rladbwls3212003@naver.com>
2025-07-02 17:27:33 +0900

    0702 - (Ws2.java 수정)

--- day01/src/WS/Ws2.java
+++ day01/src/WS/Ws2.java
@@ -7,33 +7,66 @@ import java.util.Random;
 public class Ws2 {
     public static void main(String[] args) {
         int [] arr = new int[5];
-        int max = 0;
-        int min = 11;
         Random rand = new Random();
 
-        for (int i = 0; i < 5; i++) {
-            arr[i] = rand.nextInt(10) + 1;
-            for (int j = 0; j < i; j++) {
-                if (arr[i] == arr[j]) {
-                    i--;
+        for(int i=0;i<5;i++) {
+            int num = rand.nextInt(10) +1;
+            arr[i] = num;
+            if(i == 0)
+                arr[i] = num;
+            else {
+                for (int j=0; j<i; j++) {
+                    if (arr[j] == num) {
+                        i--;
+                        break;
+                    } else {
+                        arr[i] = num;
+                    }
                 }
             }
         }
 
-        for (int i = 0; i < 5; i++) {
-            System.out.print(arr[i] + " ");
+//        for (int i = 0; i < 5; i++) {
+//            arr[i] = rand.nextInt(10) + 1;
+//            for (int j = 0; j < i; j++) {
+//                if (arr[i] == arr[j]) {
+//                    i--;
+//                }
+//            }
+//        }
+
+        System.out.println(Arrays.toString(arr));
+
+//        for (int i = 0; i < 5; i++) {
+//            System.out.print(arr[i] + " ");
+//        }
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for(int num:arr) {
+            if(num < min)
+                min = num;
+            if(num > max)
+                max = num;
         }
 
-        for (int i = 0; i < 5; i++) {
-            if (max < arr[i]) {
-                max = arr[i];
-            }
-            if (min > arr[i]) {
-                min = arr[i];
-            }
-        }
-        System.out.println();
-        System.out.println("max : " + max);
-        System.out.println("min : " + min);
+        System.out.printf("%d, %d", min, max);
+
+
+//        int max = 0;
+//        int min = 11;
+//
+//        for (int i = 0; i < 5; i++) {
+//            if (max < arr[i]) {
+//                max = arr[i];
+//            }
+//            if (min > arr[i]) {
+//                min = arr[i];
+//            }
+//        }
+//        System.out.println();
+//        System.out.println("max : " + max);
+//        System.out.println("min : " + min);
     }
 }
