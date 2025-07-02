726bcfe5a8f1708464dc029b96e853b3909f874f
Kimyoujin <rladbwls3212003@naver.com>
2025-07-02 09:21:23 +0900

    0702 - (Main2:수정)

--- day01/src/Main2.java
+++ day01/src/Main2.java
@@ -3,7 +3,7 @@ public class Main2 {
     public static void main(String[] args) {
         System.out.println("Hello!");
         for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+            System.out.println("i = " + i);
         }
     }
 }
