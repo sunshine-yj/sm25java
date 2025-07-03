4803750aa3ac5eb2d01916692b7534df407b9f31
Kimyoujin <rladbwls3212003@naver.com>
2025-07-03 13:31:29 +0900

    0703 - (Array4.java 수정, Array5.java 추가)

--- day01/src/array2/Array5.java
+++ day01/src/array2/Array5.java
@@ -1,41 +1,43 @@
 package array2;
 
-import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
-public class Array4 {
+public class Array5 {
     public static void main(String[] args) {
+        // 배열 선언
         int arr[][] = new int[3][3];
         Random rand = new Random();
-
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[i].length;j++){
+        // 배열에 값을 입력
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
                 int num = rand.nextInt(10) + 1;
                 if (i == 0 && j == 0) {
                     arr[i][j] = num;
                 } else {
                     boolean flag = true;
                     point:
+                    // num이 배열에 있으면 다시 랜덤변수 생성
                     for (int k = 0; k <= i; k++) {
-                        for (int n : arr[k]) {// arr[k]에서 원소를 하나씩 꺼내서 n에 넣는다
-                            if (n == num) {// n이 num변수와 값이 같으면 실행
+                        for(int n : arr[k]) {
+                            if (n == num) {
                                 flag = false;
                                 j--;
                                 break point;
                             }
                         }
                     }
-                    if (flag) {// flag가 참일 경우 실행
+                    // num이 배열에 없으면 입력
+                    if (flag == true) {
                         arr[i][j] = num;
                     }
                 }
-
             }
         }
-
-        for(int n[]: arr) {
-            for(int num: n) {
-                System.out.print(num + " ");
+        // 배열 출력
+        for (int a[] : arr) {
+            for (int n : a) {
+                System.out.print(n + " ");
             }
             System.out.println();
         }
