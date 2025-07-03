8abb8b9efa16b74c7efc7fa54dcb099545bdcd0e
Kimyoujin <rladbwls3212003@naver.com>
2025-07-03 09:40:00 +0900

    0703 - (Array2 package 생성, Array1.java 생성)

---
+++ day01/src/array2/Array1.java
@@ -0,0 +1,12 @@
+package array2;
+
+public class Array1 {
+    public  static void main(String[] args) {
+        int [] a = new int[5];
+        int b[] = {1,2,3,4,5};
+        int [] c = new int[]{1,2,3,4,5};
+
+        int [][] d = new int[3][3];
+
+    }
+}
