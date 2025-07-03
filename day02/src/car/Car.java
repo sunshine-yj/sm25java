62a43ab714558388a062a76eabfceeeb46d01b6e
Kimyoujin <rladbwls3212003@naver.com>
2025-07-03 16:31:13 +0900

    0703 - (day02 모듈 생성, Car.java, Main.java 생성)

---
+++ day02/src/car/Car.java
@@ -0,0 +1,15 @@
+package car;
+
+public class Car {
+    public String name;
+    public String color;
+    public int size;
+
+    public void go() {
+        System.out.println("Car go");
+    }
+
+    public void stop() {
+        System.out.println("Car stop");
+    }
+}
---
+++ day02/src/car/Main.java
@@ -0,0 +1,12 @@
+package car;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start Application...");
+        Car car = new Car();
+        car.go();
+        car.stop();
+
+        System.out.println("End Application...");
+    }
+}
