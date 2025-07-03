c5e46c0e67fb565105f6f4c3a94378196a108b8a
Kimyoujin <rladbwls3212003@naver.com>
2025-07-03 18:03:34 +0900

    0703 - (Control07.java 수정)

--- day01/src/control/Control7.java
+++ day01/src/control/Control7.java
@@ -1,5 +1,6 @@
 package control;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -110,6 +111,9 @@ public class Control7 {
                     }
                 }
 
+                for (int i = 0; i < arr3.length; i++) {
+                    System.out.println(Arrays.toString(arr3[i]));
+                }
                 // 1번 조건
                 for (int i = 0; i < arr3.length; i++) {
                     for (int j = 0; j < arr3[i].length; j++) {
