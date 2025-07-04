6d50ada0227924bf7774217236e74995e1a9b9c4
Kimyoujin <rladbwls3212003@naver.com>
2025-07-04 13:45:04 +0900

    0704 - (Company2.java 생성)

---
+++ day02/src/company/Company2.java
@@ -0,0 +1,10 @@
+package company;
+
+public class Company2 {
+    public static void main(String[] args) {
+        Employee [] e = new Employee[3];
+        e[0] = new Employee("이사원",1000.0);
+        e[1] = new SalesEmployee("이세일",1000.0,"seoul",1000.0);
+        e[2] = new SecretaryEmployee("이비서", 1000.0, "이보스");
+    }
+}
