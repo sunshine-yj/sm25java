216017e8bf1b1d995a3869b69673abf4f6ba787c
Kimyoujin <rladbwls3212003@naver.com>
2025-07-07 15:37:46 +0900

    0707 - (shop update)

---
+++ day03/src/shop/ItemService.java
@@ -0,0 +1,51 @@
+package shop;
+
+import java.util.List;
+
+public class ItemService implements SmService<ItemDto, Integer> {
+
+    SmRepository<ItemDto, Integer> smRepository;
+
+    public ItemService() {
+        smRepository = new ItemMySQLRepository();
+    }
+
+    @Override
+    public void register(ItemDto itemDto) {
+        System.out.println("ItemService: 기본정보 저장");
+        smRepository.insert(itemDto);
+        System.out.println("ItemService: SMS 전송");
+        System.out.println("ItemService: EMAIL 전송");
+        System.out.println("ItemService: 아이템 저장 완료");
+    }
+
+    @Override
+    public void modify(ItemDto itemDto) {
+        System.out.println("ItemService: 기본정보 수정");
+        smRepository.update(itemDto);
+        System.out.println("ItemService: SMS 전송");
+        System.out.println("ItemService: 아이템 정보 수정 완료");
+    }
+
+    @Override
+    public void remove(Integer integer) {
+        System.out.println("ItemService: 기본정보 제거");
+        smRepository.delete(integer);
+        System.out.println("ItemService: SMS 전송");
+        System.out.println("ItemService: 아이템 정보 삭제 완료");
+    }
+
+    @Override
+    public List<ItemDto> get() {
+        List<ItemDto> itemDtos = null;
+        itemDtos = smRepository.selectAll();
+        return itemDtos;
+    }
+
+    @Override
+    public ItemDto get(Integer integer) {
+        ItemDto itemDto = null;
+        itemDto = smRepository.select(integer);
+        return itemDto;
+    }
+}
---
+++ day03/src/shop/Main2.java
@@ -0,0 +1,38 @@
+package shop;
+
+import java.util.List;
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Start Application ...");
+        SmService<ItemDto, Integer> smService;
+        smService = new ItemService();
+
+        System.out.println("1. Item register");
+        ItemDto itemDto = new ItemDto(444, "Item_4", 4.40);
+        smService.register(itemDto);
+        System.out.println();
+
+        System.out.println("2. Item get All");
+        List<ItemDto> items = null;
+        items = smService.get();
+        for (ItemDto item : items) {
+            System.out.println(item);
+        }
+        System.out.println();
+
+        System.out.println("3. Item delete");
+        int id = 100;
+        smService.remove(id);
+        System.out.println();
+
+        System.out.println("4. Item select");
+        int id1 = 222;
+        ItemDto myInfo = null;
+        myInfo = smService.get(id1);
+        System.out.println(myInfo);
+        System.out.println();
+
+        System.out.println("End Application ...");
+    }
+}
