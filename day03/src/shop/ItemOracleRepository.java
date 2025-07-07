b441053f992298750bd0477fc35c3addc72b7b39
Kimyoujin <rladbwls3212003@naver.com>
2025-07-07 16:02:56 +0900

    0707 - (shop update)

---
+++ day03/src/shop/ItemOracleRepository.java
@@ -0,0 +1,42 @@
+package shop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemOracleRepository implements SmRepository<ItemDto, Integer> {
+
+    @Override
+    public void insert(ItemDto itemDto) {
+        System.out.println("Oracle: Inserting Item Table");
+        System.out.println("Oracle: " + itemDto);
+    }
+
+    @Override
+    public void update(ItemDto itemDto) {
+        System.out.println("Oracle: Updating Item Table");
+        System.out.println("Oracle: " + itemDto);
+    }
+
+    @Override
+    public void delete(Integer integer) {
+        System.out.println("Oracle: Deleting Item Table");
+        System.out.println("Oracle: " + integer);
+    }
+
+    @Override
+    public List<ItemDto> selectAll() {
+        System.out.println("Oracle: Selecting All Item Table");
+        List<ItemDto> itemDto = new ArrayList<>();
+        itemDto.add(new ItemDto(100, "Item_1", 1.00));
+        itemDto.add(new ItemDto(200, "Item_2", 2.00));
+        itemDto.add(new ItemDto(300, "Item_3", 3.00));
+        return itemDto;
+    }
+
+    @Override
+    public ItemDto select(Integer integer) {
+        System.out.println("Oracle: Selecting Item Table");
+        ItemDto itemDto = new ItemDto(555, "Item 5", 5.50);
+        return itemDto;
+    }
+}
--- day03/src/shop/SmRepository.java
+++ day03/src/shop/SmRepository.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 public interface SmRepository<V, K> {
     // Database에 CRUD 기능 정의
-    // Insert(C), select(R), update (U), Delete (D)
+    // Insert (C), select(R), update (U), Delete (D)
+    // Insert (C)
     void insert(V v);
+    // update (U)
     void update(V v);
+    // Delete (D)
     void delete(K k);
+    // select (R)
     List<V> selectAll();
     V select(K k);
 }
